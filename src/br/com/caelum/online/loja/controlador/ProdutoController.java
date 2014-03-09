284fa17ca46950f24e1948d812832f29048deeb7
Gil Gomes <gilgomesp@gmail.com>
2014-03-09 18:42:30 -0300

    listagem de produtos

--- src/br/com/caelum/online/loja/controlador/ProdutoController.java
+++ src/br/com/caelum/online/loja/controlador/ProdutoController.java
@@ -1,5 +1,27 @@
 package br.com.caelum.online.loja.controlador;
 
-public class ProdutoController {
+import java.util.List;
 
+import br.com.caelum.online.loja.dao.ProdutoDao;
+import br.com.caelum.online.loja.dominio.Produto;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class ProdutoController {
+	
+	private final ProdutoDao produtos;
+	
+	public ProdutoController(){
+		this.produtos = new ProdutoDao();
+	}
+	
+	public List<Produto> lista() {
+		return produtos.pegaTodos();
+	}
+	
+	@Path("/produto/{id}")
+	public Produto exibe(long id){
+		return produtos.pegaPorId(id);
+	}
 }
